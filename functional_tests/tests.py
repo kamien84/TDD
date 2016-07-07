b0cb5a954028b1c8c4c9a6de9cad1adbf60738b1
Kamil <kamien84@gmail.com>
2016-07-07 10:21:08 +0200

    make functional_tests an app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,8 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
  def setUp(self):
   self.browser = webdriver.Firefox()
   self.browser.implicitly_wait(3)
@@ -19,7 +19,7 @@ class NewVisitorTest(unittest.TestCase):
  def test_can_start_a_list_and_retrieve_it_later(self):
   # Edith has heard about a cool new online to-do app. She goes
   # to check out its homepage
-  self.browser.get('http://localhost:8000')
+  self.browser.get(self.live_server_url)
   # She notices the page title and header mention to-do lists
   self.assertIn('To-Do', self.browser.title)
   header_text = self.browser.find_element_by_tag_name('h1').text
@@ -45,7 +45,7 @@ class NewVisitorTest(unittest.TestCase):
   inputbox.send_keys(Keys.ENTER)
 
   # The page updates again, and now shows both items on her list
-  self.check_for_row_in_list_table('1: Buy a peacock feathers')
+  self.check_for_row_in_list_table('1: Buy peacock feathers')
   self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
   # Edith wonders whether the site will remember her list. Then she sees
